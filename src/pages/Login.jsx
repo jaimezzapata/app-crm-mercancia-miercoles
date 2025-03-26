7a1f932749035fb1acf029ec64312bd3d49bfa2d
jaimezzapata <zapataval2304@gmail.com>
2025-03-26 10:17:00 -0500

    Se crena enrutador con createBrowserRouter y RouterProvider

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+/* rafce -> Crea un componente funcional  flecha */
+/* rfce -> Crea un componente funcional regular */
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
