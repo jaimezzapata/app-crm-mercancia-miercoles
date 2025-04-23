4fea9dba0fd81d9ac4be09f7d71a037c685d25d3
jaimezzapata <zapataval2304@gmail.com>
2025-04-23 09:50:28 -0500

    implementación de petición get

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,78 @@
-/* rafce -> Crea un componente funcional  flecha */
-/* rfce -> Crea un componente funcional regular */
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { usuarios } from '../services/database'
-import { alertaError, alertaRedireccion, generarToken } from '../helpers/funciones'
-import './Login.css'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  alertaError,
+  alertaRedireccion,
+  generarToken,
+} from "../helpers/funciones";
+import "./Login.css";
+let apiUsuarios = "http://localhost:3000/usuarios";
+
 const Login = () => {
-  const [getUsuario, setUsuario] = useState("")
-  const [getPassword, setPassword] = useState("")
-  const [getHoraLogin, setHoraLogin] = useState(null)
-  let navigate = useNavigate()
+  const [getUsuario, setUsuario] = useState("");
+  const [getPassword, setPassword] = useState("");
+  const [getHoraLogin, setHoraLogin] = useState(null);
+  const [usuarios, setUsuarios] = useState([]);
+  let navigate = useNavigate();
+
+  function getUsuarios() {
+    fetch(apiUsuarios)
+      .then((response) => response.json())
+      .then((data) => setUsuarios(data))
+      .catch((error) => console.log(error));
+  }
+  
+  getUsuarios();
+ 
 
   function buscarUsuario() {
-    let usuarioEncontrado = usuarios.find((usuario) => getUsuario == usuario.usuario && getPassword == usuario.contrasena)
-    return usuarioEncontrado
+    let usuarioEncontrado = usuarios.find(
+      (usuario) =>
+        getUsuario == usuario.usuario && getPassword == usuario.contrasena
+    );
+    return usuarioEncontrado;
   }
 
   function inicioSesion() {
     if (buscarUsuario()) {
-      let tokenAcceso = generarToken()
-      localStorage.setItem("token", tokenAcceso)
-      alertaRedireccion(navigate, "Bienvenido " + buscarUsuario().nombre, "En breves segundos será redireccionado al Home", "success", "/home")
-      let horaInicio = new Date()
+      let tokenAcceso = generarToken();
+      localStorage.setItem("token", tokenAcceso);
+      alertaRedireccion(
+        navigate,
+        "Bienvenido " + buscarUsuario().nombre,
+        "En breves segundos será redireccionado al Home",
+        "success",
+        "/home"
+      );
+      let horaInicio = new Date();
       console.log(horaInicio);
       // setHoraLogin(horaInicio)
       // console.log(getHoraLogin);
     } else {
-      alertaError()
+      alertaError();
     }
   }
 
   return (
     <form className="form">
       Sign Up
-      <input onChange={(e) => setUsuario(e.target.value)} type="text" className="input" placeholder="Name" />
-      <input onChange={(e) => setPassword(e.target.value)} type="text" className="input" placeholder="Password" />
-      <button type='button' onClick={inicioSesion} >Submit</button>
+      <input
+        onChange={(e) => setUsuario(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Name"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Password"
+      />
+      <button type="button" onClick={inicioSesion}>
+        Submit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
