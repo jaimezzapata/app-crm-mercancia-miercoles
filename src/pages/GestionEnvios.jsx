09444b6c8db9173207511c001ba7ffac41bdf6c2
jaimezzapata <zapataval2304@gmail.com>
2025-05-14 08:59:31 -0500

    Filtrado de envios por usuario

--- src/pages/GestionEnvios.jsx
+++ src/pages/GestionEnvios.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+let apiEnvios = "http://localhost:3000/envios";
 
 const GestionEnvios = () => {
+  const [envios, setEnvios] = useState([]);
+  let usuario = JSON.parse(localStorage.getItem("usuario"))
+  function getEnvios() {
+      fetch(apiEnvios)
+        .then((response) => response.json())
+        .then((data) => setEnvios(data))
+        .catch((error) => console.log(error));
+    }
+    useEffect(() => {
+      getEnvios();
+    }, []);
+
+    function filtrarEnviosUsuario(){
+      let enviosUsuario = envios.filter((item)=>item.idUsuario == usuario.id)
+      console.log(enviosUsuario);      
+    }
+    filtrarEnviosUsuario()
+    
   return (
     <div>GestionEnvios</div>
   )
